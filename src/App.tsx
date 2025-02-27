8fd16dd0e15ce89c31a27c0c10ac31171935824b
Gustav <gustav.jorlov@telenorconnexion.com>
2025-02-27 20:10:11 +0100

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import Dashboard from './components/Dashboard'
-import './App.css'
+import Dashboard from "./components/Dashboard";
+import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <Dashboard />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
